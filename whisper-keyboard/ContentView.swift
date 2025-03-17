7f19b6f5be53c5e309e7e202895a210deab13c8f
DevelopedByDev <dlalwan1@jhu.edu>
2025-03-17 15:44:29 -0400

    Initial Commit

---
+++ whisper-keyboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  whisper-keyboard
+//
+//  Created by Divyansh Lalwani on 3/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ whisper-keyboard/whisper_keyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  whisper_keyboardApp.swift
+//  whisper-keyboard
+//
+//  Created by Divyansh Lalwani on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct whisper_keyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
